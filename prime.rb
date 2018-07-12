b1aa21651122c0d8a9581ce2731b2c4f72069474
Karla R <karla.y.reyes12@gmail.com>
2018-07-12 10:53:17 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
 # Add  code here!
+def prime?(n)
+    if n == 2:
+      return True
+    if n == 3:
+        return True
+    if n % 2 == 0:
+        return False
+    if n % 3 == 0:
+        return False
+end
